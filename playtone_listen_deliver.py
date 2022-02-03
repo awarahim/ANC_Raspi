f83afca05bb40ba581d2c2903c8c18cb701c58cf
Ainul Wadhihah Abdul Rahim <68619088+awarahim@users.noreply.github.com>
2022-02-03 13:36:05 -0500

    Update playtone_listen_deliver.py

--- playtone_listen_deliver.py
+++ playtone_listen_deliver.py
@@ -7,7 +7,9 @@ import sys
 import pyaudio
 import time
 import wave
+
 import multiprocessing as mp
+import numpy as np
 import scipy.fft as fft
 
 # Stop signal handler by Ctrl-C
@@ -29,12 +31,35 @@ FRAMES_PER_BUFFER =
 
 
 def ref_mic(p, q1, stop_event):   
-    
+   
     def callback(in_data, frame_count, time_info, status):
 #             data = rms(in_data)
+            
+            # convert the in_data into float
+            audio_data = np.fromstring(in_data, dtype=np.float32) 
+            
+            # filter to 100Hz to 1000Hz using np.fft.fft. Refer: https://www.youtube.com/watch?v=s2K1JfNR7Sc
+            n = len(audio_data)
+            fhat = np.fft.fft(audio_data,n)
+            PSD = fhat * np.conj(fhat) / n  # give the power density of all signals
+            
+            # used PSD to filter out noise
+            indices = PSD > lowPSD and PSD < highPSD  # where lowPSD is the signal with low threshold and highPSD with highthreshold (
+           
+            
+            
+            
+            # invert the filtered signal
+            
+            
+            # revert the float data into byte string 
+            
+            
             q1.put(in_data)
+            
 #             print("q1:", q1.get())
-            return in_data, pyaudio.paContinue
+            # I want to hear the inverted data
+            return out_data, pyaudio.paContinue 
         
     stream = p.open(
         format=FORMAT,
@@ -43,7 +68,7 @@ def ref_mic(p, q1, stop_event):
         frames_per_buffer=FRAMES_PER_BUFFER,
         input=True,
         input_device_index=2,
-        stream_callback = callback)
+        stream_callback=callback)
     
     while not stop_event.wait(0):
         # start stream
